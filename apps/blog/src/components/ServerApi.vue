3c3d78bdd2734eee43b263dfb77720547a4722ac
冷石Boy <coldstoneboy@qq.com>
2026-04-08 23:25:11 +0800

    feat(blog): 添加Vue支持并创建ServerApi组件
    添加@astrojs/vue集成和Vue依赖，创建ServerApi.vue组件用于展示API数据

---
+++ apps/blog/src/components/ServerApi.vue
@@ -0,0 +1,11 @@
+<script setup>
+const response = await fetch("http://localhost:3000/api/ping");
+const data = await response.json();
+</script>
+
+<template>
+  <div>
+    <h1>Server API</h1>
+    <p>{{ data }}</p>
+  </div>
+</template>
